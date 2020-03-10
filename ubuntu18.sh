271608ee4cd5db436746c5b080cc20aea881e2d6
kalyankkc <58684302+kalyankkc@users.noreply.github.com>
2020-03-10 18:10:20 +0530

    Create ubuntu18.sh

---
+++ ubuntu18.sh
@@ -0,0 +1,8 @@
+sudo apt-get install -y dirmngr gnupg
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
+sudo apt-get install -y apt-transport-https ca-certificates
+sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger bionic main > /etc/apt/sources.list.d/passenger.list'
+sudo apt-get update
+sudo apt-get install -y libapache2-mod-passenger
+sudo a2enmod passenger
+sudo apache2ctl restart
